340e39ca8d5116dff2a5e6aa8a7bbd4eb9da4e99
USER <waryz6422@gmail.com>
2026-01-21 12:20:11 +0900

    사이드바 항목 설정

--- src/pages/main.js
+++ src/pages/main.js
@@ -24,13 +24,57 @@ export default function mainPage() {
               <Collapsible defaultOpen className="group/collapsible">
                 <SidebarMenuItem>
                   <CollapsibleTrigger asChild>
-                    <SidebarMenuButton>메뉴</SidebarMenuButton>
+                    <SidebarMenuButton>설계 엔진</SidebarMenuButton>
                   </CollapsibleTrigger>
 
                   <CollapsibleContent>
                     <SidebarMenuSub>
-                      <SidebarMenuSubItem>서브메뉴 1</SidebarMenuSubItem>
-                      <SidebarMenuSubItem>서브메뉴 2</SidebarMenuSubItem>
+                      <SidebarMenuSubItem>주문 항목 생성</SidebarMenuSubItem>
+                      <SidebarMenuSubItem>시뮬레이션 결과</SidebarMenuSubItem>
+                    </SidebarMenuSub>
+                  </CollapsibleContent>
+                </SidebarMenuItem>
+              </Collapsible>
+
+              <Collapsible defaultOpen className="group/collapsible">
+                <SidebarMenuItem>
+                  <CollapsibleTrigger asChild>
+                    <SidebarMenuButton>생산 계획</SidebarMenuButton>
+                  </CollapsibleTrigger>
+
+                  <CollapsibleContent>
+                    <SidebarMenuSub>
+                      <SidebarMenuSubItem>프로덕션 타임라인</SidebarMenuSubItem>
+                    </SidebarMenuSub>
+                  </CollapsibleContent>
+                </SidebarMenuItem>
+              </Collapsible>
+
+              <Collapsible defaultOpen className="group/collapsible">
+                <SidebarMenuItem>
+                  <CollapsibleTrigger asChild>
+                    <SidebarMenuButton>게시판</SidebarMenuButton>
+                  </CollapsibleTrigger>
+
+                  <CollapsibleContent>
+                    <SidebarMenuSub>
+                      <SidebarMenuSubItem>공지사항</SidebarMenuSubItem>
+                      <SidebarMenuSubItem>자료실</SidebarMenuSubItem>
+                      <SidebarMenuSubItem>사원 게시판</SidebarMenuSubItem>
+                    </SidebarMenuSub>
+                  </CollapsibleContent>
+                </SidebarMenuItem>
+              </Collapsible>
+
+              <Collapsible defaultOpen className="group/collapsible">
+                <SidebarMenuItem>
+                  <CollapsibleTrigger asChild>
+                    <SidebarMenuButton>관리</SidebarMenuButton>
+                  </CollapsibleTrigger>
+
+                  <CollapsibleContent>
+                    <SidebarMenuSub>
+                      <SidebarMenuSubItem>사원 관리</SidebarMenuSubItem>
                     </SidebarMenuSub>
                   </CollapsibleContent>
                 </SidebarMenuItem>
@@ -39,7 +83,7 @@ export default function mainPage() {
           </SidebarContent>
         </Sidebar>
 
-        <main className="flex-1 p-6">메인 화면</main>
+        <main className="flex-1 p-6">설계 엔진</main>
       </div>
     </SidebarProvider>
   );
