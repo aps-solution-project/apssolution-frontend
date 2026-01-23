bc23c7e7b0e0aa9166917c318356810a2eb85cd5
USER <waryz6422@gmail.com>
2026-01-23 09:57:08 +0900

    auth-api  : 사원 관련 api추가(변경사항이 있음)

--- src/api/auth-api.js
+++ src/api/auth-api.js
@@ -1,7 +1,7 @@
+const URL = "http://192.168.0.17:8080";
 //로그인 api
-
 export const loginUser = async (accountId, pw) => {
-  return fetch("http://192.168.0.14:8080/api/accounts/login", {
+  return fetch(`${URL}/api/accounts/login`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -10,6 +10,118 @@ export const loginUser = async (accountId, pw) => {
   }).then((response) => {
     if (!response.ok) {
       throw new Error("사원번호와 비밀번호를 확인해주세요.");
+    }
+    return response.json();
+  });
+};
+
+//사원 추가하기
+export const createAccount = async (data) => {
+  return fetch(`${URL}/api/accounts`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  }).then((response) => {
+    if (!response.ok) {
+      throw new Error("사원 추가에 실패했습니다.");
+    }
+    return response.json();
+  });
+};
+
+//사원 정보 수정 (ADMIN전용)
+export const updateEmployeeAccount = async (accountId, data, token) => {
+  return fetch(`${URL}/api/accounts/${accountId}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify(data), // { name, role, email, workedAt }
+  }).then((response) => {
+    if (!response.ok) {
+      throw new Error("직원 정보 수정에 실패했습니다.");
+    }
+    return response.json();
+  });
+};
+
+//본인 정보 수정
+export const updateMyAccount = async (accountId, data, token) => {
+  return fetch(`${URL}/api/accounts/${accountId}/edit`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify(data),
+  }).then((response) => {
+    if (!response.ok) {
+      throw new Error("내 정보 수정에 실패했습니다.");
+    }
+    return response.json();
+  });
+};
+
+// 비밀번호 변경
+export const changeMyPassword = async (accountId, data, token) => {
+  return fetch(`${URL}/api/accounts/${accountId}/pw`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify(data),
+  }).then((response) => {
+    if (!response.ok) {
+      throw new Error("비밀번호 변경에 실패했습니다.");
+    }
+    return response.json();
+  });
+};
+
+// 사원 퇴사 처리
+export const deleteAccount = async (accountId, token) => {
+  return fetch(`${URL}/api/accounts/${accountId}/resign`, {
+    method: "PUT",
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  }).then((response) => {
+    if (!response.ok) {
+      throw new Error("퇴사 처리에 실패했습니다.");
+    }
+    return response.json();
+  });
+};
+
+//사원 전체 조회
+export const getAllAccounts = async (token) => {
+  return fetch(`${URL}/api/accounts`, {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  }).then((response) => {
+    if (!response.ok) {
+      throw new Error("사원 목록 조회에 실패했습니다.");
+    }
+    return response.json();
+  });
+};
+
+// 사원 상세 조회
+export const getAccountDetail = async (accountId, token) => {
+  return fetch(`${URL}/api/accounts/${accountId}`, {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  }).then((response) => {
+    if (!response.ok) {
+      throw new Error("사원 상세 조회에 실패했습니다.");
     }
     return response.json();
   });
--- src/api/page-api.js
+++ src/api/page-api.js
@@ -3,7 +3,7 @@ export async function upLoadFiles(file, token) {
   const formData = new FormData();
   formData.append("file", file);
 
-  const response = await fetch(" " + "/api/tasks/xls/parse", {
+  const response = await fetch("http://192.168.0.17:8080/api/tasks/xls/parse", {
     method: "POST",
     headers: {
       Token: token,
