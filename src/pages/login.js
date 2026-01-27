4b0b5f57837ff520b4f63e0e7c407cf8c6e1bab0
carino2000 <carino9231@gmail.com>
2026-01-27 15:01:03 +0900

    fix-done

---
+++ src/pages/login.js
@@ -0,0 +1,100 @@
+import { loginUser } from "@/api/auth-api";
+import { useAccount, useToken } from "@/stores/account-store";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+
+export default function LoginPage() {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const router = useRouter();
+
+  const { token, setToken, clearToken } = useToken();
+  const { account, setAccount, clearAccount } = useAccount();
+
+  const [id, setId] = useState("");
+  const [pw, setPw] = useState("");
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    if (loading) return;
+
+    setLoading(true);
+    setError(null);
+
+    try {
+      const user = await loginUser(id, pw);
+
+      setToken(user.token);
+      setAccount({
+        id: user.accountId,
+        name: user.accountName,
+        role: user.role,
+      });
+
+      const redirect = router.query.redirect || "/scenarios";
+      router.replace(redirect);
+    } catch (error) {
+      setError(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    clearToken();
+    clearAccount();
+  }, []);
+
+  return (
+    <div className="min-h-screen bg-[url('/images/login-bp-01-01.jpg')] bg-cover bg-center">
+      <div className="min-h-screen flex items-center">
+        <div className="ml-70 w-90 rounded-xl bg-white/90 px-8 py-13">
+          <div className="mb-10">
+            <h2 className="text-xl font-bold text-gray-800">로그인</h2>
+            <p className="mt-2 text-xs text-gray-500">
+              사원번호와 비밀번호를 입력해주세요.
+            </p>
+          </div>
+
+          <form className="space-y-8" onSubmit={handleLogin}>
+            <div>
+              <label className="block text-xs text-gray-500 mb-2">
+                사원번호
+              </label>
+              <input
+                value={id}
+                onChange={(e) => setId(e.target.value)}
+                className="w-full border-b pb-3 text-sm outline-none"
+                required
+              />
+            </div>
+
+            <div>
+              <label className="block text-xs text-gray-500 mb-2">
+                비밀번호
+              </label>
+              <input
+                type="password"
+                value={pw}
+                onChange={(e) => setPw(e.target.value)}
+                className="w-full border-b pb-3 text-sm outline-none"
+                required
+              />
+            </div>
+
+            {error && <p className="text-xs text-red-500">{error}</p>}
+
+            <button
+              type="submit"
+              disabled={loading}
+              className="mt-6 w-full rounded-xl bg-linear-to-r from-blue-600 via-indigo-500 to-purple-500 py-3 text-sm font-semibold text-white shadow-md hover:brightness-120 disabled:opacity-60"
+            >
+              로그인
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
